d17f4c64f8ff4ee0c2a50c20539f5c2c91797619
Martin Neburac <neburacmartin@gmail.com>
2024-02-25 20:53:09 +0100

    fewer CSS adjustments

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ particlesJS('particles',
         }
       },
       "color": {
-        "value": ["#95cf23"]
+        "value": ["#000000"] //background color 
       },
       "shape": {
         "type": "circle",
